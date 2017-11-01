1dd9c2bea983a0afffb0572d48087ccb08eb6fa9
mjmushtaq <QMaEtzz9>
2017-11-01 16:51:49 +0530

    Generation stragergy

--- src/main/java/se/cambio/training/app/AnnotationConfigApp.java
+++ src/main/java/se/cambio/training/app/AnnotationConfigApp.java
@@ -1,14 +1,10 @@
 package se.cambio.training.app;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 import org.hibernate.Session;
 
 import se.cambio.training.entities.Category;
-import se.cambio.training.entities.Invoice;
 import se.cambio.training.entities.Manufacturer;
 import se.cambio.training.entities.SparePart;
 import se.cambio.training.factories.CategoryInventoryControl;
@@ -32,34 +28,69 @@ public class AnnotationConfigApp extends AbstractApp
 		AnnotationConfigApp app = new AnnotationConfigApp("hibernate.annotated.cfg.xml");
 
 		//Begin the transaction
-		Session session = app.getCurrentSession();
+		final Session session = app.getCurrentSession();
 		session.beginTransaction();
 
-		Category category09 = app.load(Category.class,9);
-		Category category08 = app.load(Category.class,8);
-		SparePart sparePart01 = app.load(SparePart.class,1);
-		SparePart sparePart02 = app.load(SparePart.class,2);
+		final Category category = loadCategory(app, 1);
 
-		final ArrayList<SparePart> spareParts = new ArrayList<SparePart>(category09.getSpareParts());
-		spareParts.remove(sparePart01);
+		System.out.println(category);
+		System.out.println(category.getSpareParts());
 
-		category09.setSpareParts(spareParts);
-		sparePart01.setCategory(null);
-
-		final ArrayList<SparePart> spareParts02 = new ArrayList<SparePart>(category08.getSpareParts());
-		spareParts02.add(sparePart01);
-
-		category08.setSpareParts(spareParts02);
-		sparePart01.setCategory(category08);
-
-		app.persist(category08);
-		app.update(sparePart01);
-		app.persist(category09);
-		app.update(sparePart02);
+//		persistWheelsAndRims(app);
 
 		//commit the transactions
 		session.getTransaction().commit();
 		session.close();
+	}
+
+	private static void persistWheelsAndRims(AnnotationConfigApp app)
+	{
+		final Category wheels = loadCategory(app, 1);
+		final Category Rims = loadCategory(app, 2);
+
+		final Manufacturer honda = loadManufacturer(app, 1);
+		final Manufacturer suzuki = loadManufacturer(app,7);
+
+		SparePart hondaWheels = new SparePart();
+		hondaWheels.setCategory(wheels);
+		hondaWheels.setManufacturer(honda);
+		hondaWheels.setDescription("Honda Wheels for Motorbike");
+		hondaWheels.setName("Honda Wheels");
+
+		SparePart suzukiWheels = new SparePart();
+		suzukiWheels.setCategory(wheels);
+		suzukiWheels.setManufacturer(suzuki);
+		suzukiWheels.setDescription("Suzuki Wheels for Motorbike");
+		suzukiWheels.setName("Suzuki Wheels");
+
+
+		SparePart hondaRims = new SparePart();
+		hondaRims.setCategory(Rims);
+		hondaRims.setManufacturer(honda);
+		hondaRims.setDescription("Honda Rims for Motorbike");
+		hondaRims.setName("Honda Rims");
+
+		SparePart suzukiRims = new SparePart();
+		suzukiRims.setCategory(Rims);
+		suzukiRims.setManufacturer(suzuki);
+		suzukiRims.setDescription("Suzuki Rims for Motorbike");
+		suzukiRims.setName("Suzuki Rims");
+
+
+		app.persist(suzukiWheels);
+		app.persist(suzukiRims);
+		app.persist(hondaWheels);
+		app.persist(hondaRims);
+	}
+
+	private static Category loadCategory(AnnotationConfigApp app, int id)
+	{
+		return app.load(Category.class, id);
+	}
+
+	private static Manufacturer loadManufacturer(AnnotationConfigApp app, int id)
+	{
+		return app.load(Manufacturer.class, id);
 	}
 
 	private static List<Category> generateCategories()
